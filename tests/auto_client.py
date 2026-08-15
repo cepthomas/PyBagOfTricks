4a4e271263ece2410c3bbb8454fe89ecb9396316
cepthomas <cepthomas@gmail.com>
2026-08-15 17:01:57 -0500

    WIP.

---
+++ tests/auto_client.py
@@ -0,0 +1,108 @@
+import sys
+import os
+import time
+import socket
+import threading
+import queue
+import datetime
+import traceback
+
+
+HOST = '127.0.0.1'
+PORT = 59120
+
+
+#------------------------------------------------------------------------------
+class AutoTcpClient(object):
+
+    def __init__(self):
+        '''Construction.'''
+        self.sock = None
+        self.commif = None
+
+    def go(self):
+
+        print('===== Capture')
+        run = True
+
+        while run:
+
+            try:
+                print(f'Starting client on {HOST}:{PORT}')
+
+                # TCP socket client
+                self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+                # Block with timeout.
+                self.sock.settimeout(5)
+
+                self.sock.connect((HOST, PORT))
+
+                # Didn't fault so must be success.
+                self.commif = self.sock.makefile('rw')
+                print('Connected to server')
+
+                # Anything to send? Check for user input.
+                commands = ['w', 'l', 'n'] # TODO1 pass as args + port
+                while len(commands) > 0:
+
+                    smsg = commands.pop(0)
+                    self.commif.write(smsg)# + MDEL)
+                    self.commif.flush()
+                    print(f'CMD [{smsg}]')
+
+                    # Get any server responses.
+                    self.sock.settimeout(1)
+                    sresp = ''
+                    rcving = True
+                    while rcving:
+                        try:
+                            s = self.commif.read(100)
+                            # Got something.
+                            sresp += s
+
+                        except TimeoutError:
+                            # Nothing more to read.
+                            rcving = False
+
+                    # Process any capture.
+                    for s in sresp.splitlines():
+                        print(f'RSP [{s}]')
+
+                    # Delay a bit.
+                    time.sleep(0.1)
+
+                    run = False
+
+            except TimeoutError:
+                print('TimeoutError')
+
+            except ConnectionError as e:
+                # BrokenPipeError, ConnectionAbortedError, ConnectionRefusedError, ConnectionResetError.
+                print(f'ConnectionError: {type(e)}')
+
+            except KeyboardInterrupt:
+                # Hard shutdown, ignore and quit.
+                print('KeyboardInterrupt')
+
+            except Exception as e:
+                # Other unexpected error.
+                print(f'Other Error: {type(e)}')
+
+        if self.commif is not None:
+            self.commif.close()
+            self.commif = None
+
+        if self.sock is not None:
+            self.sock.close()
+            self.sock = None
+
+        print(f'=====')
+
+        sys.exit(0)
+
+
+#------------------------------------------------------------------------------
+if __name__ == '__main__':
+    client = AutoTcpClient()
+    client.go()
--- tests/test_ppdb.py
+++ tests/test_ppdb.py
@@ -1,16 +1,15 @@
-from nt import error
 import sys
 import os
-import importlib
+import bdb
 import unittest
-import threading
-import socket
-import time
-import queue
+import subprocess
 # Insert path to parent dir.
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import pbot_pdb
 import plog
+
+
+# python -m unittest test_ppdb.py
 
 # __unittest = True  # Tells unittest to completely ignore frames in this module
 
@@ -28,192 +27,64 @@ class TestPbotPdb(unittest.TestCase):
     def tearDown(self):
         plog.stop()
 
+    def function2(self, arg):
+        x = 111
+        y = 22
+        return arg + x + y
+
+    def function1(self, arg):
+        # Set a breakpoint here then step through and examine the code.
+        pbot_pdb.breakpoint()
+        return self.function2(len(arg))
+
+    def go(self, edit):
+        del edit
+
+        # Benign reload in case of edited.
+        # importlib.reload(pbot_pdb)
+
+        # Run some fake code.
+        self.function1('ABCD')
+
 
     #------------------------------------------------------------------
     def test_ppdb_tcp(self):
 
-        ### Configure ppdb. ###
+        # Configure ppdb.
         pbot_pdb.PORT = 59120
+        # pbot_pdb.USE_COLOR = True
         pbot_pdb.XLAT = {'\n': '<NL>', '\r': '<CR>', '\u001b': '<ESC>'}
         pbot_pdb.LOG_FN = os.path.join(os.path.join(os.path.dirname(__file__), 'out', 'pbot_ppdb.log'))
         try: os.remove(pbot_pdb.LOG_FN)
         except: pass
 
         commif = None
-        capture = []
+        # capture = []
 
-        ### Run simulated remote client in a thread ###
-        def worker():
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcp_socket:
+        # Run the c-u-t.
+        try:
+            self.go(None)
+        except Exception as e:
+            pass
 
-                ### Send some commands ###
-                commands = ['w', 'l', 'n']
-                while len(commands) > 0:
-                    try:
-                        ### Try connecting ###
-                        tcp_socket.settimeout(1) # enough time to start ppdb server
-                        try:
-                            tcp_socket.connect((pbot_pdb.HOST, pbot_pdb.PORT))
-                            # Didn't fault so must be success.
-                            commif = tcp_socket.makefile('rw')
-                            # plog.info('Connected to server')
+        fc = R'C:\Dev\Libs\PyBagOfTricks\tests\auto_client.py'
+        cp = subprocess.run(['py', fc], universal_newlines=True, capture_output=True, text=True, shell=True)
 
-                        except Exception as e:
-                            # plog.error('Failed to connect', e)
-                            return
-
-                        ### Next command ###
-                        smsg = commands.pop(0)
-                        plog.debug(f'Send [[[{smsg}]]]')
-                        commif.write(smsg + '\n')
-                        commif.flush()
-                        capture.append(f'S {smsg}')
-
-                        ### Listen for response(s) ###
-                        tcp_socket.settimeout(0.2)
-                        sresp = ''
-                        rcving = True
-
-                        while rcving:
-                            try:
-                                s = commif.read(100)
-                                # Got something.
-                                # plog.debug(f'worker received [[[{s}]]]')
-                                sresp += s
-
-                            except TimeoutError:
-                                # Nothing more to read.
-                                rcving = False
-
-                        # Process any capture.
-                        plog.debug(f'Receive [[[{sresp}]]]')
-                        for s in sresp.splitlines():
-                            capture.append(f'R {s}')
-
-                    except Exception as e:
-                        plog.error('Unexpected', e)
-                        capture.append(f'! {e}')
-                        # self._debug(f'CommIf.readline() exception: {str(e)}')
-                        raise # hard fail
-
-                plog.debug(f'worker loop exit')
-
-        threading.Thread(target=worker, daemon=True).start()
-
-        ### Now we can run the test code. ###
-        plog.info('Run the test code')
-        t = MyTestClass()
-        t.go()
-
-        ### Examine generated contents ###
+        # Examine generated contents
         plog.info('Examine generated contents')
+        plog.info(f'\n[{cp.stdout}]')
 
 #        for sc in capture: print('***', sc)
-        self.assertEqual(len(capture), 42)
+        # self.assertEqual(len(capture), 42)
         # lines = []
         # with open(self.log_fn) as f:
         #     lines = f.readlines()
         # self.assertEqual(len(lines), 42)
 
-        ### Stop ###
+        # Stop
         plog.info('exit')
         plog.stop()
 
         if commif is not None:
             commif.close()
             commif = None
-
-
-#-----------------------------------------------------------------------------------
-class MyTestClass():
-
-    def go(self):
-        # Set a breakpoint here then step through and examine the code.
-        pbot_pdb.breakpoint()
-
-        ret = self.klass_function_1(911, 'abcd')
-
-        # Unhandled exceptions actually go to sys.__excepthook__. Capture these in the code under test.
-        # self.klass_function_boom()
-
-        ret = self.klass_function_2([33, 'thanks', 3.56], {'aaa': 111, 'bbb': 222, 'ccc': 333})
-
-        # Run the code under debug.
-        # ret = do_it(number=911, alpha='abcd')
-
-    def klass_function_1(self, a1: int, a2: str):
-        '''A simple function.'''
-        ret = f'answer is:{a1 * len(a2)}'
-        return ret
-
-    def klass_function_2(self, a_list, a_dict):
-        '''A simple function.'''
-        return len(a_list) + len(a_dict)
-
-    def klass_function_boom(self):
-        '''A function that causes an unhandled exception.'''
-        return 1 / 0
-
-
-#-----------------------------------------------------------------------------------
-# Other test code that may be useful TODO1
-#-----------------------------------------------------------------------------------
-
-
-# #-----------------------------------------------------------------------------------
-# class MyClassA(object):
-#     '''A simple debug target.'''
-
-#     def __init__(self, name, tags, arg):
-#         self._name = name
-#         self._tags = tags
-#         self._arg = arg
-
-#     def do_something(self, arg):
-#         res = f'{self._arg}-user-{arg}'
-#         return res
-
-#     def do_boom(self):
-#         # Cause unhandled exception.
-#         return 1 / 0
-
-# #----------------------------------------------------------
-# def function_1(a1: int, a2: float):
-#     '''A simple function.'''
-#     cl1 = MyClassA('number 1', [45, 78, 23], a1)
-#     cl2 = MyClassA('number 2', [100, 101, 102], a2)
-#     ret = f'answer is cl1:{cl1.do_something(a1)}...cl2:{cl2.do_something(a2)}'
-
-#     # Play with exception handling.
-#     # ret = f'{cl1.do_boom()}'
-
-#     return ret
-
-# #----------------------------------------------------------
-# def function_2(a_list, a_dict):
-#     '''A simple function.'''
-#     return len(a_list) + len(a_dict)
-
-# #----------------------------------------------------------
-# def function_boom():
-#     '''A function that causes an unhandled exception.'''
-#     return 1 / 0
-
-# #----------------------------------------------------------
-# def do_it(alpha, number):
-#     '''Main code.'''
-
-#     # Benign reload in case of being edited.
-#     # importlib.reload(pbot_pdb)
-
-#     # Set a breakpoint here then step through and examine the code.
-#     pbot_pdb.breakpoint()
-
-#     ret = function_1(number, len(alpha))
-
-#     # Unhandled exception actually goes to sys.__excepthook__.
-#     function_boom()
-
-#     ret = function_2([33, 'thanks', 3.56], {'aaa': 111, 'bbb': 222, 'ccc': 333})
-
-#     return ret
